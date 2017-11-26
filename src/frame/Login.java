d5b742fa6c292aedd67bd50b8d5f88b132b93fba
abdulazizalreshidi <abdulazizalreshidi@abdulazizs-mbp.lan>
2017-11-26 15:57:47 -0600

    all file

---
+++ src/frame/Login.java
@@ -0,0 +1,14 @@
+ package frame;
+
+public class Login {
+ 
+    public static boolean authenticate(String username, String password) {
+        // hardcoded username and password
+        if (username.equals("Aziz") && password.equals("123")) 
+        	 return true;
+        if(username.equals("m") && password.equals("123")){
+            return true;
+        }
+        return false;
+    }
+}
