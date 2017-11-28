b35a4047b26ebc74cec6d249d2636152a1b39b5a
sleng0022 <leng.sinith001@gmail.com>
2017-11-27 19:46:19 -0600

    Add log in frame.

---
+++ src/frame/Register.java
@@ -0,0 +1,20 @@
+ package frame;
+
+public class Register {
+	private int registerId;
+	private String registerName;
+	
+	public int getRegisterId() {
+		return registerId;
+	}
+	public void setRegisterId(int registerId) {
+		this.registerId = registerId;
+	}
+	
+	public String getRegisterName() {
+		return registerName;
+	}
+	public void setRegisterName(String registerName) {
+		this.registerName = registerName;
+	}	
+}
